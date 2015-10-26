d182353389c92cf0087b66899b7c5691983ec885
fey <federica.recanatini@gmail.com>
2015-10-26 15:12:49 +0100

    setup

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var path = require('path')
+var nodeModules = path.resolve(__dirname, 'node_modules');
+var config = {
+  entry: path.resolve(__dirname, 'app/main.js'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/, 
+      loader: 'babel',
+      exclude: nodeModules
+    }]
+  }
+};
+
+module.exports = config;
