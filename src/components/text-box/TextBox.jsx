e541ad87dfc4647b43023bce2327ab6fd7d1ba94
Claudio Brandolino <cbrandolino@gmail.com>
2015-11-14 16:37:45 +0100

    Implemented js style standards

--- src/components/text-box/TextBox.jsx
+++ src/components/text-box/TextBox.jsx
@@ -1,11 +1,12 @@
-import React, {Component} from 'react'
-import './text-box.scss'
+import React, { Component } from 'react';
+import './text-box.scss';
 
 export default class TextBox extends Component {
+
   render() {
     return (
       <div className="text-box--text">
-        {`The subject who is under the ascendancy of the Imagerepertoire
+        { `The subject who is under the ascendancy of the Imagerepertoire
         "offers" nothing in the play of the signifier: he
         dreams little, never pens. If he writes, his writing is
         smooth as an Image, always seeking to reinstate a legible
@@ -13,8 +14,9 @@ export default class TextBox extends Component {
         to the modern text-which, a contrario, is defined by the
         abolition of the Image-repertoire: nothing "novelistic," no
         simulated Image: for Imitation, Representation, and
-        Analogy are forms of coalescence: outmoded.`}
+        Analogy are forms of coalescence: outmoded.` }
       </div>
-    )
+    );
   }
+
 }
