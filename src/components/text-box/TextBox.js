5bc391dc19e85dbb381ae8e21cd0f25c96e46038
fey <federica.recanatini@gmail.com>
2015-11-14 14:11:06 +0100

    text-box as component, renamed folder to node standards

---
+++ src/components/text-box/TextBox.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+import './text-box.scss'
+
+export default class TextBox extends Component {
+  render() {
+    return (
+      <div className="text-box--text">
+        {`The subject who is under the ascendancy of the Imagerepertoire
+        "offers" nothing in the play of the signifier: he
+        dreams little, never pens. If he writes, his writing is
+        smooth as an Image, always seeking to reinstate a legible
+        surface of the words: anachronistic, in short, with regard
+        to the modern text-which, a contrario, is defined by the
+        abolition of the Image-repertoire: nothing "novelistic," no
+        simulated Image: for Imitation, Representation, and
+        Analogy are forms of coalescence: outmoded.`}
+      </div>
+    )
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
 import './style/main.scss'
+import TextBox from './components/text-box/TextBox'
+
+main()
+
+function main() {
+  ReactDOM.render(<TextBox />, document.getElementById('text-box'));
+}
